58983c8b1d70b9b3782beecce1750c2120447fa5
Abhishek Patro <agbpatro@gmail.com>
2015-09-21 17:23:23 -0700

    Anaglyph scene

---
+++ Anaglyph/Assets/Scripts/AnaglyphCamera.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnaglyphCamera : MonoBehaviour {
+	
+	
+	private		RenderTexture 		leftEyeRT;   
+	private 	RenderTexture		rightEyeRT;
+	private		GameObject	 		leftEye;
+	private		GameObject	 		rightEye;
+	public 		Material	 		anaglyphMat;
+
+	void Start () {
+
+		if ( anaglyphMat == null ) {
+			Debug.LogError("No Material Found Please Drag The material in the appropriate Field");
+			this.enabled = false;
+			return;
+		}
+
+		leftEye = new GameObject ("leftEye", typeof ( Camera ) );
+		rightEye = new GameObject ("rightEye", typeof ( Camera ) );
+
+		leftEye.GetComponent<Camera>().CopyFrom (GetComponent<Camera>());
+		rightEye.GetComponent<Camera>().CopyFrom (GetComponent<Camera>());
+		
+		leftEyeRT = new RenderTexture (Screen.width, Screen.height, 24);
+		rightEyeRT = new RenderTexture (Screen.width, Screen.height, 24);
+		
+		leftEye.GetComponent<Camera>().targetTexture = leftEyeRT;
+		rightEye.GetComponent<Camera>().targetTexture = rightEyeRT;
+		
+		anaglyphMat.SetTexture ("_LeftTex", leftEyeRT);
+		anaglyphMat.SetTexture ("_RightTex", rightEyeRT);
+		
+		leftEye.GetComponent<Camera>().depth = GetComponent<Camera>().depth -2;
+		rightEye.GetComponent<Camera>().depth = GetComponent<Camera>().depth -1;
+		
+		leftEye.transform.position = transform.position + transform.TransformDirection(-0.8f, 0f, 0f);
+		rightEye.transform.position = transform.position + transform.TransformDirection(0.8f, 0f, 0f);
+		
+		leftEye.transform.parent = transform;
+		rightEye.transform.parent = transform;
+		
+		GetComponent<Camera>().cullingMask = 0;
+		GetComponent<Camera>().backgroundColor = new Color (0f,0f,0f,0f);
+		GetComponent<Camera>().clearFlags = CameraClearFlags.Nothing;
+	}
+	
+	void Stop () {
+	}
+	
+	
+	void OnRenderImage ( RenderTexture source, RenderTexture destination ) {
+		Graphics.Blit (source, destination, anaglyphMat);
+	}
+	
+}
+
+
+
---
+++ Anaglyph/Assets/Shader/AnaglyphShader.shader
@@ -0,0 +1,40 @@
+﻿Shader "Custom/AnaglyphShader" {
+Properties {
+   _LeftTex ("Left (RGB)", RECT) = "white" {}
+   _RightTex ("Right (RGB)", RECT) = "white" {}
+}
+
+SubShader {
+   Pass {
+      ZTest Always Cull Off ZWrite Off
+      Fog { Mode off }
+
+      CGPROGRAM
+	 #pragma vertex vert_img
+      #pragma fragment frag
+      #pragma fragmentoption ARB_precision_hint_fastest
+      #include "UnityCG.cginc"
+      
+      uniform sampler2D _LeftTex;
+      uniform sampler2D _RightTex;
+      
+      
+      half4 frag (v2f_img i) : COLOR
+      {
+         float r,g,b;
+         float4 texL = tex2D(_LeftTex, i.uv);
+         float4 texR = tex2D(_RightTex, i.uv);
+         float4 texRGB;
+        r = texL.r;
+        g = texR.g;
+        b = texR.b;
+        
+         
+         texRGB = float4(r,g,b,1);
+         return texRGB;
+      }
+      ENDCG
+   }
+}   
+   Fallback off
+} 
